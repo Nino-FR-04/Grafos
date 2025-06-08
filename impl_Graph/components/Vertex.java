82acc0cf2cfde91667544ff1e80a68e256e2096f
Nino-FR-04 <davidfrx322@gmail.com>
2025-06-08 12:49:14 -0500

    Correccion - Clase Vertex

--- impl_Graph/components/Vertex.java
+++ impl_Graph/components/Vertex.java
@@ -2,8 +2,16 @@ package impl_Graph.components;
 
 import impl_List.ListLinked;
 
+/**
+ * Representa un vertice (vertex) en un grafo.
+ *
+ * @param E el tipo de dato almacenado en los vértices del grafo.
+ */
 public class Vertex <E extends Comparable<E>> implements Comparable<Vertex<E>> {
     
+    /**
+     * Estado que puede tomar un vertice durante un recorrido.
+     */
     public enum VertexState {
         UNEXPLORED,
         VISITED
@@ -12,16 +20,22 @@ public class Vertex <E extends Comparable<E>> implements Comparable<Vertex<E>> {
     //Atributos
     protected E data;
     protected ListLinked<Edge<E>> listadj;
+    protected VertexState state;
 
     //Constructor
     public Vertex(E data) {
         this.data = data;
         this.listadj = new ListLinked<Edge<E>>();
+        this.state = VertexState.UNEXPLORED;
     }
 
-    //Setters y Getters
+    //Metodos -> Setters y Getters
     public E getData() {return this.data;}
+    public void setData(E data) {this.data = data;}
     public ListLinked<Edge<E>> getListAdj() {return this.listadj;}
+    public VertexState getVertexState() {return this.state;}
+    public void setVertexState(VertexState state) {this.state = state;}
+    
 
     //Equals
     @Override
@@ -37,5 +51,11 @@ public class Vertex <E extends Comparable<E>> implements Comparable<Vertex<E>> {
     @Override
     public int compareTo(Vertex<E> o) {
         return this.data.compareTo(o.data);
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return this.data + " -> " + this.listadj.toString() + "\n";
     }
 }
